1dbcaf2dd4879b577d1aa3583b5ef68e165a83b5
Ivory Sherman <ivorysherman@Ivorys-MacBook-Pro.local>
2019-10-13 21:07:56 -0400

    working answer feedback screens

--- js/app.js
+++ js/app.js
@@ -1,225 +1,228 @@
 (function(scs) {
-    scs(window.jQuery, window, document);
-    }(function($, window, document) {
+  scs(window.jQuery, window, document);
+  }(function($, window, document) {
 
-        // `quizQuestions` is responsible for storing the underlying data
-        // that our app needs to keep track of in order to work.
-        //quizQuestions should have the question(s), the answer(s), the CORRECT answer with textual feedback 
+      // `quizQuestions` is responsible for storing the underlying data
+      // that our app needs to keep track of in order to work.
+      //quizQuestions should have the question(s), the answer(s), the CORRECT answer with textual feedback 
 
-        const quizQuestions = [
-            {
-              question: "On average, how many aluminum soda cans are used in the United States each year?",
+      const quizQuestions = [
+          {
+            question: "On average, how many aluminum soda cans are used in the United States each year?",
+            answers: {
+              a: "5 billion",
+              b: "70 billion",
+              c: "80 billion", 
+              d: "95 billion"
+            },
+            correctAnswer: "80 billion",
+            answerFeedback: "We use over 80 billion aluminum soda cans every year, which are recyclable, can be made into a variety of aluminum products."
+          },
+          {
+            question: "How many gallons of motor gasoline does the average American consume in one year?",
+            answers: {
+              a: "150 gallons",
+              b: "500 gallons",
+              c: "610 gallons",
+              d: "750 gallons"
+            },
+            correctAnswer: "500 gallons",
+            answerFeedback: "The average American consumes approximately 500 gallons of gasoline per year. Carpooling & taking alternate means of transportation can help cut down on gasoline consumption!"
+          },
+          {
+            question: "How long would it take for a glass bottle to decompose in a dump?",
+            answers: {
+              a: "250 years",
+              b: "500 years",
+              c: "3000 years",
+              d: "4000 years"
+            },
+            correctAnswer: "4000 years", 
+            answerFeedback: "It will take the typical glass bottle 4000 years or more to decompose."
+          },
+          {
+              question: "How much does the average American consume in paper, wood, and other products made from trees each year?",
               answers: {
-                a: "5 billion",
-                b: "70 billion",
-                c: "80 billion", 
-                d: "95 billion"
+                a: "7 trees",
+                b: "9 trees",
+                c: "35 trees",
+                d: "50 trees"
               },
-              correctAnswer: "c",
-              answerFeedback: "We use over 80 billion aluminum soda cans every year, which are recyclable, can be made into a variety of aluminum products."
-            },
-            {
-              question: "How many gallons of motor gasoline does the average American consume in one year?",
+              correctAnswer: "7 trees", 
+              answerFeedback: " The average American uses seven trees a year in paper, wood, and other products made from trees. Throughout the United States this amounts to about 2 billion trees per year!"
+          },
+          {
+              question: "What percentage of monetary purchasing goes to packaging materials?",
               answers: {
-                a: "150 gallons",
-                b: "500 gallons",
-                c: "610 gallons",
-                d: "750 gallons"
+                a: "$1 for every $10 spent",
+                b: "$1 for every $50 spent",
+                c: "$1 for every $150 spent",
+                d: "$1 for every $500 spent"
               },
-              correctAnswer: "b",
-              answerFeedback: "The average American consumes approximately 500 gallons of gasoline per year. Carpooling & taking alternate means of transportation can help cut down on gasoline consumption!"
-            },
-            {
-              question: "How long would it take for a glass bottle to decompose in a dump?",
-              answers: {
-                a: "250 years",
-                b: "500 years",
-                c: "3000 years",
-                d: "4000 years"
-              },
-              correctAnswer: "d", 
-              answerFeedback: "It will take the typical glass bottle 4000 years or more to decompose."
-            },
-            {
-                question: "How much does the average American consume in paper, wood, and other products made from trees each year?",
-                answers: {
-                  a: "7 trees",
-                  b: "9 trees",
-                  c: "35 trees",
-                  d: "50 trees"
-                },
-                correctAnswer: "a", 
-                answerFeedback: " The average American uses seven trees a year in paper, wood, and other products made from trees. Throughout the United States this amounts to about 2 billion trees per year!"
-            },
-            {
-                question: "What percentage of monetary purchasing goes to packaging materials?",
-                answers: {
-                  a: "$1 for every $10 spent",
-                  b: "$1 for every $50 spent",
-                  c: "$1 for every $150 spent",
-                  d: "$1 for every $500 spent"
-                },
-                correctAnswer: "a", 
-                answerFeedback: "$1 out of every $10 spent at stores is for packaging. Packaging accounts for one-third of our waste by weight or half of our waste by volume."
-            }
-        ];
+              correctAnswer: "$1 for every $10 spent", 
+              answerFeedback: "$1 out of every $10 spent at stores is for packaging. Packaging accounts for one-third of our waste by weight or half of our waste by volume."
+          }
+      ];
 
-        const totalQuestions = quizQuestions.length; //5
+      const totalQuestions = quizQuestions.length; //5
 
-        //Set the current question to display to 1
-        let currentQuestion = 0;
+      //Set the current question to display to 1
+      let currentQuestion = 0;
+      
+      let currentScore = 0;
+      //need to add +1 if question is answered correctly 
+      
+      function generateItemsString() {
+        //console.log(quizQuestion);
         
-        let currentScore = 0;
-        //need to add +1 if question is answered correctly 
+        //to get question(s)--> console.log(quizQuestion[0].question);
+        //to get answers--> console.log(quizQuestion[0].answers);
+        //to get correctAnswer --> console.log(quizQuestion[0].correctAnswer);
+        //to get feedback--> console.log(quizQuestion[0].answerFeedback);
+
+        //console.log(quizQuestion[0].correctAnswer);
+
+        var quizQuestionContent = [];
+
+
+        quizQuestions.forEach(function (quizQuestion, index) {
+          //console.log(quizQuestion.correctAnswer);
+          //quizQuestionContent.push(quizQuestion.question);
+          //console.log(quizQuestion.question, index);
+          //console.log(quizQuestion.answers.a);
+
+          quizQuestionContent.push(
+            `<h2 class="quiz-screen__question">${quizQuestion.question}</h2>
+            <form class="form">
+                <div class="inputGroup">
+                  <input id="${quizQuestion.answers.a}" name="radio" type="radio" value="${quizQuestion.answers.a}"/>
+                  <label for="${quizQuestion.answers.a}">${quizQuestion.answers.a}</label>
+                </div>
+                <div class="inputGroup">
+                  <input id="${quizQuestion.answers.b}" name="radio" type="radio" value="${quizQuestion.answers.b}"/>
+                  <label for="${quizQuestion.answers.b}">${quizQuestion.answers.b}</label>
+                </div>
+                <div class="inputGroup">
+                  <input id="${quizQuestion.answers.c}" name="radio" type="radio" value="${quizQuestion.answers.c}"/>
+                  <label for="${quizQuestion.answers.c}">${quizQuestion.answers.c}</label>
+                </div>
+                <div class="inputGroup">
+                  <input id="${quizQuestion.answers.d}" name="radio" type="radio" value="${quizQuestion.answers.d}"/>
+                  <label for="${quizQuestion.answers.d}">${quizQuestion.answers.d}</label>
+                </div>
+            </form>`
+          );
+        });
         
-        function generateItemsString() {
-          //console.log(quizQuestion);
-          
-          //to get question(s)--> console.log(quizQuestion[0].question);
-          //to get answers--> console.log(quizQuestion[0].answers);
-          //to get correctAnswer --> console.log(quizQuestion[0].correctAnswer);
-          //to get feedback--> console.log(quizQuestion[0].answerFeedback);
+        //console.log(quizQuestionContent[currentQuestion]);
 
-          var quizQuestionContent = [];
+        return `${quizQuestionContent[currentQuestion]}`;
+      }
 
+      function renderQuizApp() {
+          // this function will be responsible for rendering the quiz app in the DOM
+          console.log('`renderQuizApp` ran');
+          const quizQuestionString = generateItemsString();
 
-          quizQuestions.forEach(function (quizQuestion, index) {
-            //console.log(quizQuestion);
-            //quizQuestionContent.push(quizQuestion.question);
-            //console.log(quizQuestion.question, index);
-            //console.log(quizQuestion.answers.a);
+          // insert that HTML into the DOM
+          $('#quiz__content').html(quizQuestionString);
+      }
 
-            quizQuestionContent.push(
-              `<h2 class="quiz-screen__question">${quizQuestion.question}</h2>
-              <form class="form">
-                  <div class="inputGroup">
-                    <input id="${quizQuestion.answers.a}" name="radio" type="radio" value="${quizQuestion.answers.a}"/>
-                    <label for="${quizQuestion.answers.a}">${quizQuestion.answers.a}</label>
-                  </div>
-                  <div class="inputGroup">
-                    <input id="${quizQuestion.answers.b}" name="radio" type="radio" value="${quizQuestion.answers.b}"/>
-                    <label for="${quizQuestion.answers.b}">${quizQuestion.answers.b}</label>
-                  </div>
-                  <div class="inputGroup">
-                    <input id="${quizQuestion.answers.c}" name="radio" type="radio" value="${quizQuestion.answers.c}"/>
-                    <label for="${quizQuestion.answers.c}">${quizQuestion.answers.c}</label>
-                  </div>
-                  <div class="inputGroup">
-                    <input id="${quizQuestion.answers.d}" name="radio" type="radio" value="${quizQuestion.answers.d}"/>
-                    <label for="${quizQuestion.answers.d}">${quizQuestion.answers.d}</label>
-                  </div>
-              </form>`
-            );
+      function startQuizApp() {
+          //responsible for when an user clicks a button to start the quiz
+          console.log('`startQuizApp` ran');
+
+          //on button click, hide start screen
+          //show first question
+
+          $("#start-button").click(function(event) {
+            event.preventDefault();
+            
+            $("#quiz-screen-intro").hide();
+            $("#quiz").show();
+
+            /*$("#quiz").show().animate({
+              opacity: 1
+            }, 500);*/
           });
-          
-          //console.log(quizQuestionContent[0]);
+      }
 
-          return `${quizQuestionContent[currentQuestion]}`;
-        }
+      function quizAppProgress() {
+          //responsible for letting the user know what question they are on
+          // i.e. Question: 2/5
 
-        function renderQuizApp() {
-            // this function will be responsible for rendering the quiz app in the DOM
-            console.log('`renderQuizApp` ran');
-            const quizQuestionString = generateItemsString();
+          //Need a loop to add +1 and stop at 5 
 
-            // insert that HTML into the DOM
-            $('#quiz__content').html(quizQuestionString);
-        }
+          console.log('`quizAppProgress` ran');
+      }
 
-        function startQuizApp() {
-            //responsible for when an user clicks a button to start the quiz
-            console.log('`startQuizApp` ran');
+      function currentQuizScore() {
+          //responsible for letting the user know what their CURRENT score is
+          //i.e. Score: 2
 
-            //on button click, hide start screen
-            //show first question
+          console.log('`currentQuizScore` ran');
+      }
 
-            $("#start-button").click(function(event) {
-              event.preventDefault();
-              
-              $("#quiz-screen-intro").hide();
-              $("#quiz").show();
+      function onUserAnswerSubmission() {
+          //responsible for giving textual feedback about their answer. 
+          //If incorrect, they should be told the correct answer.
+          //user should also be able to move onto the next question.
 
-              /*$("#quiz").show().animate({
-                opacity: 1
-              }, 500);*/
-            });
-        }
-
-        function handleQuizQuestions() {
-            //responsible for user answering one question at a time 
-            //should not be able to skip questions 
-
-            //how to dynamically put quiz questions and answer choices into div from array
-            //console.log(quizQuestions);
-
-            console.log('`handleQuizQuestions` ran');
-        }
-
-        function quizAppProgress() {
-            //responsible for letting the user know what question they are on
-            // i.e. Question: 2/5
-
-            //Need a loop to add +1 and stop at 5 
-
-            console.log('`quizAppProgress` ran');
-        }
-
-        function currentQuizScore() {
-            //responsible for letting the user know what their CURRENT score is
-            //i.e. Score: 2
-
-            console.log('`currentQuizScore` ran');
-        }
-
-        $("#submit").click(function(event) {
+          $("#submit").click(function(event) {
             event.preventDefault();
 
-            onUserAnswerSubmission();
-            hideThis();
-            renderQuizApp();
-        });
+            let userAnswer = $("input:checked").val();
+            console.log(userAnswer);
 
-        function hideThis() {
-          $(".wrp").hide();
-          $("#quiz-feedback").show();
-        }
+            if (userAnswer === quizQuestions[currentQuestion].correctAnswer) {
+              $(".quiz-screen__answer-header").html('Correct!');
+            } else {
+              $(".quiz-screen__answer-header").html('Incorrect!');
+            }
 
-        function onUserAnswerSubmission() {
-            //responsible for giving textual feedback about their answer. 
-            //If incorrect, they should be told the correct answer.
-            //user should also be able to move onto the next question.
+            $(".quiz-screen__answer-copy").html(quizQuestions[currentQuestion].answerFeedback);
+            $(".quiz__content-wrapper").hide();
+            $("#quiz-feedback").show();
+          });
 
+          $("#next").click(function(event) {
+            event.preventDefault();
+
+            $(".quiz__content-wrapper").show();
+            $("#quiz-feedback").hide();
+            
             currentQuestion+=1; 
-
-            console.log('`onUserAnswerSubmission` ran');
-        }
-        
-        function overallQuizScore() {
-            //responsible for displaying how many questions the user got right 
-            //out of total questions asked
-            //i.e. You got x out of 5 right! 
-
-            console.log('`overallQuizScore` ran');
-        }
-
-        function restartQuizApp() {
-            //responsible for when an user wants to start a new quiz
-            console.log('`restartQuizApp` ran');
-        }
-
-        // this function will be our callback when the page loads.
-        function handleQuizApp() {
             renderQuizApp();
-            startQuizApp();
-            handleQuizQuestions();
-            quizAppProgress();
-            currentQuizScore();
-            onUserAnswerSubmission();
-            overallQuizScore();
-            restartQuizApp();
-        }
+          });
 
-        // when the page loads, call `handleQuizApp`
-        $(handleQuizApp);
-    })
+          console.log('`onUserAnswerSubmission` ran');
+      }
+
+      function overallQuizScore() {
+          //responsible for displaying how many questions the user got right 
+          //out of total questions asked
+          //i.e. You got x out of 5 right! 
+
+          console.log('`overallQuizScore` ran');
+      }
+
+      function restartQuizApp() {
+          //responsible for when an user wants to start a new quiz
+          console.log('`restartQuizApp` ran');
+      }
+
+      // this function will be our callback when the page loads.
+      function initQuiz() {
+          renderQuizApp();
+          startQuizApp();
+          quizAppProgress();
+          currentQuizScore();
+          onUserAnswerSubmission();
+          overallQuizScore();
+          restartQuizApp();
+      }
+
+      // when the page loads, call `handleQuizApp`
+      $(initQuiz);
+  })
 );
--- js/to-do.js
+++ js/to-do.js
@@ -1,9 +1,22 @@
 //To-Do list
 
-//Check if last question and go to result screen
-//User should not be able to skip to next question/on click, user goes to next question
-//Correct answer (compare answers) and feedback screen
+//Correct answer (compare answers) and feedback screen - DONE
 
+//User should not be able to skip to next question/on click, user goes to next question
+//Score should update +1 if user gets answer correct
+//Question should update +1 when user goes to next question (update after user clicks next)
+//Check if last question and go to result screen
+//Restart quiz 
+//Beautify quiz
 
 //WANT TO REFACTOR 
 //generateItemsString();
+
+
+//thoughts to self
+//--get value of selected answer: COMPLETE
+    //user answer must equal `quizQuestion.answers.c`
+//--compare if it is correct or incorrect
+//--if correct; say correct! & add +1 to score
+//--if incorrect; say incorrect & do not add to score
+//.inputGroup input:checked ~ label:before
